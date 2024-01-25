95c594a4ef71258c85e9afbf56d457a40456a250
jmulligan <sungspoon804323@gmail.com>
2024-01-25 15:09:23 -0600

    Revert "updated admin to use __all__ (futureproof)"
    This reverts commit e69f7e93fdf3fee75dd8776cbbfbd79277f587d6.

--- competitions/admin.py
+++ competitions/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
-from .models import __all__
+from .models import *
 
-for cls in __all__:
-    admin.site.register(cls)
-
-# admin.site.register(Organization)
-# admin.site.register(Team)
-# admin.site.register(Competition)
-# admin.site.register(Event)
-# admin.site.register(Ranking)
-# admin.site.register(SingleEliminationTournament)
-# # admin.site.register(DoubleEliminationTournament)
-# # admin.site.register(MultilevelTournament)
-# # admin.site.register(RoundRobinTournament)
-# admin.site.register(Match)
+admin.site.register(Organization)
+admin.site.register(Team)
+admin.site.register(Competition)
+admin.site.register(Event)
+admin.site.register(Ranking)
+admin.site.register(SingleEliminationTournament)
+# admin.site.register(DoubleEliminationTournament)
+# admin.site.register(MultilevelTournament)
+# admin.site.register(RoundRobinTournament)
+admin.site.register(Match)
