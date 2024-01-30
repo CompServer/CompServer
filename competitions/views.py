c60beba0bb3ee4c1cd0aec0accf398af87c27134
ngyangakoto <ngyangakoto@ucls.uchicago.edu>
2024-01-29 20:43:22 -0600

    hw:team page view and model

--- competitions/views.py
+++ competitions/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 
+def team_page(team_id):
+    context = {
+        team: Team.objects.all().filter(Team.id == team_id), #get a team from the team id passed into the view
+    }
+    return render("competitions/team-page.html")
