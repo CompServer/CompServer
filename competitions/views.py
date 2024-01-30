48ecc7272326100e046884994b400f87d502400d
mriggle <mriggle@ucls.uchicago.edu>
2024-01-30 13:43:50 -0600

    prototype, 2^n teams, 1-4 teams in a match

--- competitions/views.py
+++ competitions/views.py
@@ -1,2 +1,48 @@
+import math
+import random
 from django.shortcuts import render
 
+def BracketView(request):
+    t = ""
+
+    numTeams = 8
+    numRounds = int(math.log(numTeams, 2))
+
+    roundWidth = 150
+    bracketWidth = (roundWidth+30)*numRounds
+
+    bracketHeight = 600
+    roundHeight = bracketHeight
+
+    t += f'<div class="bracket" style="height: {bracketHeight}px; width: {bracketWidth}px;">'    
+
+    for i in range(0,numRounds):
+
+        t += f'<div class="round" style="height: {roundHeight}px; width: {roundWidth}px;">'
+
+        numMatches = 2**(numRounds-i-1)
+        matchHeight = roundHeight/numMatches
+        matchWidth = roundWidth
+
+        for j in range(1, numMatches+1):
+
+            numTeams = 2 if i!=0 else 4
+            teamHeight = 25
+            centerHeight = teamHeight*numTeams
+            topPadding = (matchHeight-centerHeight)/2
+
+            t += f'<div class="match" style="height: {matchHeight}px; width: {matchWidth}px;">'
+            
+            t += f'<div class="center" style="height: {centerHeight}px; padding-top: {topPadding}px">'
+
+            for k in range(0,numTeams):
+                t += f'<div class="team" style="width: {matchWidth}px;">Team</div>'
+            
+            t += f'</div></div>'
+
+        t += '</div>'
+
+    t += '</div>'
+
+    context = {"content":t,}
+    return render(request, "competitions/bracket.html", context)
