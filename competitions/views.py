b59e356e6a115ab91b4d8ab80ad9d3338936ad54
hfeng <hfeng@ucls.uchicago.edu>
2024-01-30 14:09:02 -0600

    cw

--- competitions/views.py
+++ competitions/views.py
@@ -5,13 +5,14 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.views.generic.edit import UpdateView
-from .models import Organization, Team, Competition, Event, Ranking, SingleEliminationTournament, Match
+from .models import Organization, Team, Competition, Event, Ranking, SingleEliminationTournament, Match, Status
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 def competitions(request):
     competition_list = Competition.objects.all()
-    return render(request, "competitions/competition_hub.html", {"competition_list": competition_list})
+    context = {"competition_list": competition_list, "Status": Status}
+    return render(request, "competitions/competition_hub.html", context)
 
 def not_implemented(request, *args, **kwargs):
     messages.error(request, "This feature is not yet implemented.")
