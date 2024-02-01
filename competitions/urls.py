4df3eb48ffa39f9fc44071d56cd32c86a11b459b
omaryadams <138074975+omaryadams@users.noreply.github.com>
2024-02-01 14:20:12 -0600

    connecting code

--- competitions/urls.py
+++ competitions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path("competitions/<int:pk>", views.not_implemented, name="competition"),
     path("team/<int:team_id>/", views.team_page, name="team-page"),
     # path("coach/<int:coach_id>/", views.coach_page, name="coach-page"),
+    path("", views.comp, name="comp")
 ]
