98ba4bcf73c994684e16f32299ead5c85823034c
jmulligan <jmulligan@ucls.uchicago.edu>
2024-02-14 13:06:21 -0600

    testing

--- competitions/forms.py
+++ competitions/forms.py
@@ -1,6 +1,7 @@
 
 
 from django import forms
+from django.contrib import messages
 
 from competitions.models import Team, Match
 
@@ -10,6 +11,15 @@ class JudgeForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if possible_advancers:
             self.fields['advancers'].queryset = possible_advancers
+
+    def is_valid(self):
+        assert isinstance(self.instance, Match)
+        print(self.instance.advancers.all())
+        print(self.fields['advancers'].queryset)
+        for team in self.instance.advancers.all():
+            if team not in self.fields['advancers'].queryset:
+                return False
+        return super().is_valid()
 
     class Meta:
         model = Match
--- competitions/tests.py
+++ competitions/tests.py
@@ -1,1 +1,15 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from .models import *
+#c.post('/match/21/judge',)
+
+
+class MyTestCase(TestCase):
+
+    def test1(self):
+        print('running')
+
+        c = Client()
+
+        c.login(username="admin", password="admin")
+
+        self.assertFalse(c.post("/match/85/judge/", {"advancers": 25}))
