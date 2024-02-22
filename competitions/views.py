99c44a83575b9aa3b4156786f48dba563b28a230
ngyangakoto <ngyangakoto@ucls.uchicago.edu>
2024-02-22 14:35:57 -0600

    cw:debugging funcs

--- competitions/views.py
+++ competitions/views.py
@@ -331,14 +331,29 @@ def set_timezone_view(request: HttpRequest):
 def team(request, team_id):
     today = timezone.now().date()
     upcoming_matches = Match.objects.filter(Q(starting_teams__id=team_id) | Q(prev_matches__advancers__id=team_id), tournament__competition__start_date__lte=today, tournament__competition__end_date__gte=today, advancers=None).order_by("time")
-    won_matches = Match.objects.filter(Q(advancers__id = team_id), advancers.count() == 1).order_by("time")
-    draw_matches = Match.objects.filter(Q(advancers__id = team_id), advancers.count() > 1).order_by("time")
-    lost_matches = Match.objects.filter(Q(starting_teams__id = team_id), Q(advancers__id != team_id)).order_by("time")
+    past_matches = Match.objects.filter(starting_teams__id = team_id, tournament__status = Status.COMPLETE)
+    won_matches = past_matches.filter(advancers__id = team_id).annotate("advancers")
+    #new_matches = won_matches.filter(advancers__count = 1)).order_by("time")
+    draw_matches = past_matches.exclude(won_matches).order_by("time")
+    lost_matches = past_matches.filter(Q(starting_teams__id = team_id), Q(advancers__id != team_id)).order_by("time")
+    past_tournaments = Tournament.objects.filter(Q(teams__id = team_id), is_complete())
+    won_tournaments = []
+    if past_tournaments.exists():
+        for tournament in past_tournaments:
+            for match in past_tournaments.match_set():
+                if team not in match.advancers:
+                    break
+            won_tournaments.append(tournament)
+    past_competitions = Competition.objects.filter(Q(teams__id = team_id), is_complete()).order_by("end_date")
     context = {
         'team': Team.objects.get(pk=team_id),
         'upcoming_matches': upcoming_matches,
         'wins_list': won_matches,
         'draws_list': draw_matches,
         'losses_list': lost_matches,
+        'won_tournaments': won_tournaments,
+        #how do you order tournaments
+        'past_tournaments': past_tournaments,
+        'past_competitions': past_competitions,
     }
     return render(request, "competitions/team.html", context)
