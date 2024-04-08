a7d2fcfb702a09cd11fc28d198aef7dd1783a6e6
hfeng <hfeng@ucls.uchicago.edu>
2024-04-07 22:18:34 -0500

    hw, determined byes automatically, continued to debug creation and editing of tournaments (everything on my end works as intended)

--- competitions/views.py
+++ competitions/views.py
@@ -215,7 +215,11 @@ def generate_round_robin_matches(request, tournament_id):
     starting_time = tournament.start_time 
     teams = [team for team in tournament.teams.all()]
     teams_played = {team: set() for team in teams}
+    num_byes = len(teams) % tournament.teams_per_match
+    byes = [0 for _ in range(len(teams))]
     for k in range(tournament.num_rounds):
+        if byes == [1 for _ in range(len(teams))]:
+            byes = [0 for _ in range(len(teams))]
         nmpt_iterator = 0
         if arena_iterator > 0:
             arena_iterator = 0
@@ -224,6 +228,35 @@ def generate_round_robin_matches(request, tournament_id):
         temp = min(teams_played.values(), key=lambda x: len(x))
         if temp == len(teams) - 1:
             teams_played = {team: set() for team in teams}
+
+        #create bye match
+        match = Match.objects.create(tournament=tournament)
+        for i in range(num_byes):
+            j = random.randint(0, len(teams)-1)
+            while(byes[j] > 0 or teams[j] in match.starting_teams.all() or \
+            isPlayed(teams_played[teams[j]], match.starting_teams.all())):
+                j = random.randint(0, len(teams)-1)    
+            match.starting_teams.add(teams[j])      
+            byes[j] += 1
+            num_participated[j] += 1 
+        for team in match.starting_teams.all():
+            for team2 in match.starting_teams.all():
+                if team != team2:
+                    teams_played[team].add(team2)
+        #starting time confusion for byes (do we have them)
+        match.time = starting_time
+        match.arena = arenas[arena_iterator]
+        nmpt_iterator += 1
+        if nmpt_iterator == arenas[arena_iterator].capacity:
+            arena_iterator += 1
+            nmpt_iterator = 0
+            if arena_iterator >= len(arenas):
+                arena_iterator = 0
+                starting_time += tournament.event.match_time 
+        match.round_num = k+1
+        match.save()
+    
+        #create rest of the matches
         while num_participated != [1 for _ in range(len(teams))]:
             match = Match.objects.create(tournament=tournament)
             for i in range(tournament.teams_per_match):
