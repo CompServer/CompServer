d2963aa74678edfa1bbe269e4bec7d334ac0715f
mriggle <mriggle@ucls.uchicago.edu>
2024-04-18 11:13:22 -0500

    hw: added round headers

--- competitions/views.py
+++ competitions/views.py
@@ -473,10 +473,11 @@ def single_elimination_tournament(request: HttpRequest, tournament_id: int):
 
     # -----VARIABLES-----
     bracket_array = []
-    matchWidth = 175
+    matchWidth = 200
     connectorWidth = 50
     teamHeight = 25
     roundWidth = matchWidth + connectorWidth
+    roundNames = ["Quarter Finals", "Semi Finals", "Finals"]
     # -------------------
 
     def generate_competitor_data(match):
@@ -596,7 +597,9 @@ def single_elimination_tournament(request: HttpRequest, tournament_id: int):
     bracketWidth = (matchWidth + connectorWidth ) * numRounds
     bracketHeight = mostTeamsInRound * teamHeight * 2
 
-    for round_matches in reversed(bracket_array):
+    namedRoundCutoff = len(bracket_array) - len(roundNames)
+    for round, round_matches in enumerate(reversed(bracket_array)):
+
         num_matches = len(round_matches)
         match_height = bracketHeight / num_matches
         final_match_data = []
@@ -611,12 +614,12 @@ def single_elimination_tournament(request: HttpRequest, tournament_id: int):
                 "team_data": generated_match_data[0] if generated_match_data else None,
                 "connector_data": generated_match_data[1] if generated_match_data else None,
                 "match_height": match_height,
-                "match_width": matchWidth,
                 "center_height": center_height,
                 "center_top_margin": (match_height - center_height) / 2,
             })
 
-        round_data.append({"match_data": final_match_data})
+        label = "Round " + str(round+1) if round < namedRoundCutoff else roundNames[round - namedRoundCutoff]
+        round_data.append({"label": label,"match_data": final_match_data})
 
 
     bracket_dict = {
@@ -626,12 +629,13 @@ def single_elimination_tournament(request: HttpRequest, tournament_id: int):
         "roundHeight": bracketHeight,
         "teamHeight": teamHeight,
         "connectorWidth": connectorWidth,
+        "match_width": matchWidth,
         "round_data": round_data,
         "team_height": teamHeight,
         "championship_id": championship.id,
         "champion_id": championship.advancers.first().id if championship.advancers.first() else None
     }
-    
+
     tournament = get_object_or_404(SingleEliminationTournament, pk=tournament_id)
     context = {"tournament": tournament, "bracket_dict": bracket_dict, "form": TournamentStatusForm()}
     return render(request, "competitions/bracket.html", context)
