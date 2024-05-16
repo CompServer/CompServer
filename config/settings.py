32845bea01564ab4f62dd12571ea9edd077cc78f
jmulligan <sungspoon804323@gmail.com>
2024-05-16 11:31:14 -0500

    translation + demo changes

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ import environ
 import google.auth
 from google.cloud import secretmanager
 import io
+import yaml
 
 # custom logging filter to suppress certain errors (such as Forbidden and Not Found)
 
@@ -44,61 +45,70 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, True))
 env_file = os.path.join(BASE_DIR, ".env")
 
-# Attempt to load the Project ID into the environment, safely failing on error.
-try:
-    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default() # type: ignore
-except google.auth.exceptions.DefaultCredentialsError:
-    pass
 
-if os.path.isfile(env_file):
-    # Use a local secret file, if provided
+PROD = env("PROD", default=False)
 
-    env.read_env(env_file)
-# [START_EXCLUDE]
-elif os.getenv("TRAMPOLINE_CI", None):
-    # Create local settings if running with CI, for unit testing
+if PROD:
 
-    placeholder = (
-        f"SECRET_KEY=a\n"
-        "GS_BUCKET_NAME=None\n"
-        f"DATABASE_URL=sqlite://{os.path.join(BASE_DIR, 'db.sqlite3')}"
-    )
-    env.read_env(io.StringIO(placeholder))
-# [END_EXCLUDE]
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+    # Attempt to load the Project ID into the environment, safely failing on error.
+    try:
+        _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default() # type: ignore
+    except google.auth.exceptions.DefaultCredentialsError:
+        pass
 
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+    if os.path.isfile(env_file):
+        # Use a local secret file, if provided
 
-    env.read_env(io.StringIO(payload))
+        env.read_env(env_file)
+    # [START_EXCLUDE]
+    elif os.getenv("TRAMPOLINE_CI", None):
+        # Create local settings if running with CI, for unit testing
+
+        placeholder = (
+            f"SECRET_KEY=a\n"
+            "GS_BUCKET_NAME=None\n"
+            f"DATABASE_URL=sqlite://{os.path.join(BASE_DIR, 'db.sqlite3')}"
+        )
+        env.read_env(io.StringIO(placeholder))
+    # [END_EXCLUDE]
+    elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+        # Pull secrets from Secret Manager
+        project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+        client = secretmanager.SecretManagerServiceClient()
+        settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+        name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+        payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+        env.read_env(io.StringIO(payload))
+    else:
+        raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+    SECRET_KEY = env("SECRET_KEY")
+    SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('GOOGLE_CLIENT_ID')
+    SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('GOOGLE_CLIENT_SECRET')
 else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
-# [END cloudrun_django_secret_config]
-SECRET_KEY = env("SECRET_KEY")
+    if os.path.exists('secrets.yml'):
+        with open('secrets.yml') as f:
+            config = dict(yaml.safe_load(f))
+            SECRET_KEY = config["SECRET_KEY"]
+            SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config['GOOGLE_CLIENT_ID']
+            SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config['GOOGLE_CLIENT_SECRET']
+    else:
+        raise Exception("No local .env or secrets.yml detected. No secrets found.")
 
-DEBUG = env("DEBUG")
+assert SECRET_KEY is not None
+    #recommended by https://cloud.google.com/python/django/appengine
+#     env = Env(
+#         DEBUG=(bool, False),
+#         PROD=(bool, False),
+#         DEMO=(bool, False),
+#   )
 
-DEMO = env('DEMO')
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('GOOGLE_CLIENT_ID')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('GOOGLE_CLIENT_SECRET')
 
 
-# if os.path.exists('secrets.yml'):
-#     with open('secrets.yml') as f:
-#         config = dict(yaml.safe_load(f))
-#         SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config['GOOGLE_CLIENT_ID']
-#         SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config['GOOGLE_CLIENT_SECRET']
-# recommended by https://cloud.google.com/python/django/appengine
-# env = Env(
-#     DEBUG=(bool, False),
-#     PROD=(bool, False),
-#     DEMO=(bool, False),
-# )
+DEBUG = env("DEBUG", default=False)
+
+DEMO = env('DEMO', default=False)
 
 
 # https://cloud.google.com/python/django/appengine
@@ -127,12 +137,15 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('GOOGLE_CLIENT_SECRET')
 # SECURITY WARNING: It's recommended that you use this when
 # running in production. The URL will be known once you first deploy
 # to Cloud Run. This code takes the URL and converts it to both these settings formats.
-CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
-if CLOUDRUN_SERVICE_URL:
-    ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
-    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+if PROD:
+    CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
+    if CLOUDRUN_SERVICE_URL:
+        ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
+        CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
+        SECURE_SSL_REDIRECT = True
+        SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    else:
+        ALLOWED_HOSTS = ["*"]
 else:
     ALLOWED_HOSTS = ["*"]
 
@@ -236,12 +249,20 @@ AUTHENTICATION_BACKENDS = [
 #             'NAME': BASE_DIR / 'db.sqlite3',
 #         }
 #   }
-DATABASES = {"default": env.db()}
 
-if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
-    DATABASES["default"]["HOST"] = "127.0.0.1"
-    DATABASES["default"]["PORT"] = 5432
+if PROD:
+    DATABASES = {"default": env.db()}
 
+    if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+        DATABASES["default"]["HOST"] = "127.0.0.1"
+        DATABASES["default"]["PORT"] = 5432
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 STORAGES = {
     "default": {
